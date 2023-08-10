f4d69dec557ad78272bdf7994af304f0d6bfc31e
Emil Dandanell Agerschou <emildandanell.agerschou@ibt.cas.cz>
2023-08-10 14:19:37 +0200

    Committed automatically after model mbdb-spr has been compiled
    omb-00147-after-model-compile-mbdb-spr

---
+++ models/mbdb-spr/mbdb_spr/config.py
@@ -0,0 +1,31 @@
+from mbdb_spr.resources.files.config import MbdbSprFileResourceConfig
+from mbdb_spr.resources.files.resource import MbdbSprFileResource
+from mbdb_spr.resources.records.config import MbdbSprResourceConfig
+from mbdb_spr.resources.records.resource import MbdbSprResource
+from mbdb_spr.services.files.config import MbdbSprFileServiceConfig
+from mbdb_spr.services.files.service import MbdbSprFileService
+from mbdb_spr.services.records.config import MbdbSprServiceConfig
+from mbdb_spr.services.records.service import MbdbSprService
+
+MBDB_SPR_RECORD_RESOURCE_CONFIG = MbdbSprResourceConfig
+
+
+MBDB_SPR_RECORD_RESOURCE_CLASS = MbdbSprResource
+
+
+MBDB_SPR_RECORD_SERVICE_CONFIG = MbdbSprServiceConfig
+
+
+MBDB_SPR_RECORD_SERVICE_CLASS = MbdbSprService
+
+
+MBDB_SPR_FILES_RESOURCE_CONFIG = MbdbSprFileResourceConfig
+
+
+MBDB_SPR_FILES_RESOURCE_CLASS = MbdbSprFileResource
+
+
+MBDB_SPR_FILES_SERVICE_CONFIG = MbdbSprFileServiceConfig
+
+
+MBDB_SPR_FILES_SERVICE_CLASS = MbdbSprFileService
