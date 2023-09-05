c3ea0a0470e7c90e037f2e407eb1323b0d0c5567
Mirek Simek <miroslav.simek@gmail.com>
2023-09-05 15:02:30 +0200

    Committed automatically after model mbdb-spr has been compiled
    omb-00217-after-model-compile-mbdb-spr

--- models/mbdb-spr/mbdb_spr/files/api.py
+++ models/mbdb-spr/mbdb_spr/files/api.py
@@ -1,9 +1,18 @@
+from invenio_drafts_resources.records.api import ParentRecord
 from invenio_pidstore.providers.recordid_v2 import RecordIdProviderV2
 from invenio_records_resources.records.api import FileRecord
 from invenio_records_resources.records.systemfields import IndexField
 from invenio_records_resources.records.systemfields.pid import PIDField, PIDFieldContext
 
-from mbdb_spr.files.models import MbdbSprFileMetadata
+from mbdb_spr.files.models import DraftParentMetadata, MbdbSprFileMetadata, ParentState
+
+
+class DraftParentRecord(ParentRecord):
+    model_cls = DraftParentMetadata
+
+    # schema = ConstantField(
+    #    "$schema", "local://parent-v1.0.0.json"
+    # )
 
 
 class MbdbSprFileIdProvider(RecordIdProviderV2):
@@ -20,3 +29,7 @@ class MbdbSprFile(FileRecord):
     )
     dumper_extensions = []
     record_cls = None  # is defined inside the parent record
+
+    versions_model_cls = ParentState
+
+    parent_record_cls = DraftParentRecord
--- models/mbdb-spr/mbdb_spr/records/models.py
+++ models/mbdb-spr/mbdb_spr/records/models.py
@@ -1,15 +1,42 @@
 from invenio_db import db
+from invenio_drafts_resources.records import (
+    DraftMetadataBase,
+    ParentRecordMixin,
+    ParentRecordStateMixin,
+)
 from invenio_files_rest.models import Bucket
 from invenio_records.models import RecordMetadataBase
 from sqlalchemy_utils import UUIDType
 
 
-class MbdbSprMetadata(db.Model, RecordMetadataBase):
+class DraftParentMetadata(db.Model, RecordMetadataBase):
+    __tablename__ = "mbdb_spr_parent_metadata"
+
+
+class MbdbSprMetadata(db.Model, RecordMetadataBase, ParentRecordMixin):
     """Model for MbdbSprRecord metadata."""
 
     __tablename__ = "mbdbspr_metadata"
 
     # Enables SQLAlchemy-Continuum versioning
     __versioned__ = {}
+
+    __parent_record_model__ = DraftParentMetadata
     bucket_id = db.Column(UUIDType, db.ForeignKey(Bucket.id))
     bucket = db.relationship(Bucket)
+
+
+class MbdbSprDraftMetadata(db.Model, DraftMetadataBase, ParentRecordMixin):
+    """Model for MbdbSprDraft metadata."""
+
+    __tablename__ = "mbdbsprdraft_metadata"
+
+    __parent_record_model__ = DraftParentMetadata
+
+
+class ParentState(db.Model, ParentRecordStateMixin):
+    table_name = "parent_state_metadata"
+
+    __parent_record_model__ = DraftParentMetadata
+    __record_model__ = MbdbSprMetadata
+    __draft_model__ = MbdbSprDraftMetadata
