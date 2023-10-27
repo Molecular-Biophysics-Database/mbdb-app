9aadf98e798b2d099c962e16ab54d39d686a2234
Emil Dandanell Agerschou <emildandanell.agerschou@ibt.cas.cz>
2023-10-27 16:48:06 +0200

    updated ui-form to version 0.0.20 which support general parameters version 0.9.13

--- ui/mbdb-mst-ui/mbdb_mst_ui/theme/webpack.py
+++ ui/mbdb-mst-ui/mbdb_mst_ui/theme/webpack.py
@@ -13,7 +13,7 @@ theme = WebpackThemeBundle(
             },
             dependencies={
                 "react-searchkit": "^2.0.0",
-                "@mbdb/input-form": "^0.0.19",
+                "@mbdb/input-form": "^0.0.20",
             },
             devDependencies={},
             aliases={
