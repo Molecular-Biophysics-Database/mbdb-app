f5a8126f17c1015380e9cc28aa9ecf2257029cad
Mirek Simek <miroslav.simek@gmail.com>
2023-11-12 15:46:24 +0100

    test scripts

---
+++ scripts/run-tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+nrp upgrade
+nrp build
+
+nrp invenio db init
+nrp invenio db create
+nrp invenio index init
+nrp oarepo cf init
+nrp oarepo fixtures load
+nrp invenio files location create --default default file:////tmp/data
+nrp oarepo fixtures load --no-system-fixtures sample_data/mst
+
+
+nrp run 2>&1 | tee nrp-server.log &
+NRP_SERVER_PID=$!
+
+trap "kill $NRP_SERVER_PID" EXIT
+
+sleep 5
+
+curl -X https://127.0.0.1:5000/api/mbdb-mst
