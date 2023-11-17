77784e043ab0cf1857cea8304c4c6836f5a1a38a
Mirek Simek <miroslav.simek@gmail.com>
2023-11-17 13:22:07 +0100

    Committed automatically before model mbdb-mst has been compiled
    omb-00291-before-model-compile-mbdb-mst

--- models/mbdb-mst/mbdb_mst/services/files/service.py
+++
@@ -1,9 +0,0 @@
-from invenio_records_resources.services import FileService
-
-
-class MbdbMstFileService(FileService):
-    """MbdbMstFile service."""
-
-
-class MbdbMstFileDraftService(FileService):
-    """MbdbMstFileDraft service."""
