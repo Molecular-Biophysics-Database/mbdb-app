8c2ab32b8886d6643dd8b81350048f4e2aadada1
Mirek Simek <miroslav.simek@gmail.com>
2024-02-01 07:37:33 +0100

    luqum query str param scaffolded

--- common/services/records/params/luqum_query_str_param.py
+++ common/services/records/params/luqum_query_str_param.py
@@ -7,6 +7,9 @@ class LuqumQueryStrParam(QueryStrParam):
     config: SearchOptions
 
     def apply(self, identity, search, params):
+        print("LUQUM query str param called")
+        mapping = self.get_mapping()
+        analyzer = self.get_schema_analyzer(mapping)
         # mapping_name = self.config.mapping_name - for now, keep it on search options
         # mapping_file = current_search.mappings[mapping_name]
         # call luqum here
@@ -14,3 +17,9 @@ class LuqumQueryStrParam(QueryStrParam):
         # update search
         # and remove this
         return super().apply(identity, search, params)
+
+    def get_mapping(self):
+        return None
+
+    def get_schema_analyzer(self, mapping):
+        return None
