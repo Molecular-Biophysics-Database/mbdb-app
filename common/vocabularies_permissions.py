554bd5aacbed11618b006fdaced8db89f6a7d3c9
Emil Dandanell Agerschou <emildandanell.agerschou@ibt.cas.cz>
2024-03-11 15:08:29 +0100

    Fine grained permission test

---
+++ common/vocabularies_permissions.py
@@ -0,0 +1,6 @@
+from invenio_records_permissions.generators import AuthenticatedUser, SystemProcess
+from oarepo_runtime.services.config.permissions_presets import ReadOnlyPermissionPolicy, EveryonePermissionPolicy
+
+
+class FineGrainedVocabularyPermissionPolicy(ReadOnlyPermissionPolicy):
+    can_create_chemicals = [AuthenticatedUser(), SystemProcess()]
