f6479149220c879069f346cf0b7f95e498e70eca
Emil Dandanell Agerschou <emildandanell.agerschou@ibt.cas.cz>
2024-03-12 16:40:51 +0100

    allowed systemprocces to create specific vocabularies

--- common/vocabularies_permissions.py
+++ common/vocabularies_permissions.py
@@ -4,5 +4,8 @@ from oarepo_runtime.services.config.permissions_presets import ReadOnlyPermissio
 
 class FineGrainedVocabularyPermissionPolicy(ReadOnlyPermissionPolicy):
     can_create_chemicals = [AuthenticatedUser(), SystemProcess()]
+    can_create_affiliations = [SystemProcess()]
+    can_create_grants = [SystemProcess()]
+    can_create_organisms = [SystemProcess()]
+    can_create_instruments = [SystemProcess()]
     can_create = [AuthenticatedUser(), SystemProcess()]
-
