7b20c53a29846d30cdcce4e0a8116d0804778b49
Terezie <terezie.prchalova1@gmail.com>
2024-05-31 14:03:43 +0200

    Update

--- ui/components/semantic-ui/js/mbdb/forms/deposit/buildingBlocks/CreateOptions.js
+++ ui/components/semantic-ui/js/mbdb/forms/deposit/buildingBlocks/CreateOptions.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 function CreateOptions(list, label) {
     if (list && list.some(item => item.name !== undefined)) {
         return list.map(item => ({ value: item.name, label: item.name }));
