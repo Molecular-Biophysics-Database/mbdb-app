99d65ebf48dd581ec88bd5960941e9698db88a9a
Terezie <terezie.prchalova1@gmail.com>
2024-06-26 16:14:55 +0200

    flow -> path visible

--- ui/spr/semantic-ui/js/spr/forms/deposit/measurementProtocol/Path.js
+++ ui/spr/semantic-ui/js/spr/forms/deposit/measurementProtocol/Path.js
@@ -4,30 +4,35 @@ import ArrayField from "@mbdb_deposit/buildingBlocks/ArrayField";
 import { useFormikContext, getIn } from "formik";
 import CreateOptions from "@mbdb_deposit/buildingBlocks/CreateOptions";
 
-function Path( { name } ) {
+function Path({ name }) {
+  const { values } = useFormikContext();
 
-    const { values } = useFormikContext();
-
-    const measurementPositionValue = getIn(values, 'metadata.method_specific_parameters.measurement_positions');
-    const measurementPositionOptions = CreateOptions(measurementPositionValue, 'Select Measurement position, if applicable');
+  const measurementPositionValue = getIn(
+    values,
+    "metadata.method_specific_parameters.measurement_positions"
+  );
+  const measurementPositionOptions = CreateOptions(
+    measurementPositionValue,
+    "Select Measurement position, if applicable"
+  );
 
   return (
     <>
-        <div className="-mt-3">
-            <ArrayField
-                name={name}
-                label='Measurement position'
-                tooltip='Name (id) of the measurement position'
-                renderChild={({ arrayName, index }) => (
-                    <OptionField
-                        name={`${arrayName}.${index}.name`}
-                        label={`Measurement position`}
-                        options={measurementPositionOptions}
-                        tooltip='Name (id) of the measurement position'
-                    />
-                )}
+      <div className="-mt-3">
+        <ArrayField
+          name={name}
+          label="Measurement position"
+          tooltip="Name (id) of the measurement position"
+          renderChild={({ arrayName, index }) => (
+            <OptionField
+              name={`${arrayName}.${index}.name`}
+              label={`Measurement position`}
+              options={measurementPositionOptions}
+              tooltip="Name (id) of the measurement position"
             />
-        </div>
+          )}
+        />
+      </div>
     </>
   );
 }
--- ui/spr/semantic-ui/js/spr/forms/deposit/measurements/Measurements.js
+++ ui/spr/semantic-ui/js/spr/forms/deposit/measurements/Measurements.js
@@ -10,89 +10,87 @@ import OptionalField from "@mbdb_deposit/buildingBlocks/OptionalField";
 import CreateOptions from "@mbdb_deposit/buildingBlocks/CreateOptions";
 import CreateUuid from "@mbdb_deposit/buildingBlocks/CreateUuid";
 
-function Measurements( { name } ) {
+function Measurements({ name }) {
+  const { values } = useFormikContext();
 
-    const { values } = useFormikContext();
+  UseDefault(`${name}.samples`, [{}]);
 
-    UseDefault(`${name}.sample`, [{}]);
+  const measurementPotionValue = getIn(
+    values,
+    `metadata.method_specific_parameters.measurement_positions`
+  );
+  const measurementPotionOptions = CreateOptions(
+    measurementPotionValue,
+    "Select Measurement position, if applicable"
+  );
 
-    const measurementPotionValue = getIn(values, `metadata.method_specific_parameters.measurement_positions`);
-    const measurementPotionOptions = CreateOptions(measurementPotionValue, 'Select Measurement position, if applicable');
-
-    CreateUuid(name);
+  CreateUuid(name);
 
   return (
     <>
-        <div className="mb-3">
-            <CustomField
-                name={name}
-                fieldName='name'
-                label='Name'
-                required={true}
-                tooltip='Name (id) of the measurement which must be unique within a record (i.e. triplicates must be named individually in the raw data file). The name must allow location of the measurement data within the raw data file as well as processed data files if these are present'
-                width='w-full'
-            />
+      <div className="mb-3">
+        <CustomField
+          name={name}
+          fieldName="name"
+          label="Name"
+          required={true}
+          tooltip="Name (id) of the measurement which must be unique within a record (i.e. triplicates must be named individually in the raw data file). The name must allow location of the measurement data within the raw data file as well as processed data files if these are present"
+          width="w-full"
+        />
+      </div>
+      <div className="flex mb-3">
+        <div className="mr-3">
+          <OptionField
+            name={name}
+            label="Measurement position"
+            fieldName="measurement_position"
+            required={true}
+            options={measurementPotionOptions}
+          />
         </div>
-        <div className="flex mb-3">
-            <div className="mr-3">
-                <OptionField
-                    name={name}
-                    label='Measurement position'
-                    fieldName='measurement_position'
-                    required={true}
-                    options={measurementPotionOptions}
-                />
-            </div>
-            <div className="-mt-3">
-                <OptionalField
-                    name={name}
-                    fieldName='reference_measurement_position'
-                    label='Reference measurement positon'
-                    renderChild={({ optionalFieldName }) => (
-                        <OptionField
-                            name={optionalFieldName}
-                            label='Reference measurement position'
-                            options={measurementPotionOptions}
-                        />
-                    )}
-                />
-            </div>
+        <div className="-mt-3">
+          <OptionalField
+            name={name}
+            fieldName="reference_measurement_position"
+            label="Reference measurement positon"
+            renderChild={({ optionalFieldName }) => (
+              <OptionField
+                name={optionalFieldName}
+                label="Reference measurement position"
+                options={measurementPotionOptions}
+              />
+            )}
+          />
         </div>
-        <div>
-            <ArrayField
-                name={name}
-                fieldName='samples'
-                label='Samples'
-                required={true}
-                renderChild={({ arrayName, index }) => (
-                    <FormWrapper
-                        headline={`Sample ${index + 1}`}
-                        colorSchema='light'
-                    >
-                        <Sample
-                            name={`${arrayName}.${index}`}
-                        />
-                    </FormWrapper>
-                )}
-            />
-        </div>
-        <div>
-            <ArrayField
-                name={name}
-                fieldName='reference_sample'
-                label='Reference sample'
-                renderChild={({ arrayName, index }) => (
-                    <FormWrapper
-                        headline={`Reference sample ${index + 1}`}
-                        colorSchema='light'
-                    >
-                        <Sample
-                            name={`${arrayName}.${index}`}
-                        />
-                    </FormWrapper>
-                )}
-            />
-        </div>
+      </div>
+      <div>
+        <ArrayField
+          name={name}
+          fieldName="samples"
+          label="Samples"
+          required={true}
+          renderChild={({ arrayName, index }) => (
+            <FormWrapper headline={`Sample ${index + 1}`} colorSchema="light">
+              <Sample name={`${arrayName}.${index}`} />
+            </FormWrapper>
+          )}
+        />
+      </div>
+      <div>
+        <ArrayField
+          name={name}
+          fieldName="reference_sample"
+          label="Reference sample"
+          renderChild={({ arrayName, index }) => (
+            <FormWrapper
+              headline={`Reference sample ${index + 1}`}
+              colorSchema="light"
+            >
+              <Sample name={`${arrayName}.${index}`} />
+            </FormWrapper>
+          )}
+        />
+      </div>
     </>
   );
 }
