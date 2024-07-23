f9d42865a225f9a8006341307e87192794d5507a
Terezie <terezie.prchalova1@gmail.com>
2024-07-23 13:40:04 +0200

    Vocabularies

--- ui/branding/semantic-ui/templates/oarepovocabularies/form/FormFieldsContainer.jsx
+++ ui/branding/semantic-ui/templates/oarepovocabularies/form/FormFieldsContainer.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { TextField, FieldLabel } from "react-invenio-forms";
 import {
   PropFieldsComponent,
 } from "@js/oarepo_vocabularies";
 import { useFormConfig } from "@js/oarepo_ui";
-import { i18next } from "@translations/oarepo_vocabularies_ui/i18next";
 import _has from "lodash/has";
-import TitleField from "./TitleField";
 import { useFormikContext } from "formik";
 import { v4 as uuidv4 } from 'uuid';
 import { useEffect } from "react";
+import CustomField from "./CustomField";
 
 const VocabularyFormFields = () => {
   const {
@@ -27,7 +25,7 @@ const VocabularyFormFields = () => {
   return (
     <div>
       <div className="mb-4">
-        <TitleField
+        <CustomField
           name='title'
           label='Title'
           fieldName='en'
@@ -36,18 +34,14 @@ const VocabularyFormFields = () => {
       </div>
       {type === "chemicals" &&
       <>
-        <TitleField
-          name='id'
-          label='ID'
-          fieldName='inchikey'
-        />
-        <TextField
-          fieldPath="id"
-          label={
-            <FieldLabel htmlFor="id" label={i18next.t("ID")} />
-          }
-          required
-        />
+        <div className="mb-6">
+          <CustomField
+            name='id'
+            label='ID'
+            prefix='inchikey'
+            required
+          />
+        </div>
       </>
       }
       {hasPropFields && (
