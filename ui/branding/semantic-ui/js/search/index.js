1b860d9c5875707ae8a912b87b771b76f0778d5e
Emil Dandanell Agerschou <emildandanell.agerschou@ibt.cas.cz>
2024-08-14 14:56:17 +0200

    created js implementation of the duplicate record functionality

--- ui/branding/semantic-ui/js/search/index.js
+++ ui/branding/semantic-ui/js/search/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ChooseSearchMethodDropdown from "./ChooseSearchMethodDropdown";
 import DeleteButton from "./DeleteButton";
+import TemplateButton from "./TemplateButton";
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const depositButton = document.getElementById("search-dropdown");
   const deleteButton = document.getElementById("delete-button");
-
+  const templateButton = document.getElementById("template-button");
   if (depositButton) {
     ReactDOM.render(<ChooseSearchMethodDropdown />, depositButton);
   }
@@ -14,5 +16,9 @@ document.addEventListener("DOMContentLoaded", () => {
   if (deleteButton) {
     const selfLink = deleteButton.getAttribute("data-selflink");
     ReactDOM.render(<DeleteButton selfLink={selfLink} />, deleteButton);
+  }
+  if (templateButton) {
+    const selfLink = templateButton.getAttribute("data-selflink");
+    ReactDOM.render(<TemplateButton selfLink={selfLink} />, templateButton);
   }
 });
