fa0cd9597addf05dcb812fde5095e2180e7416c2
Emil Dandanell Agerschou <emildandanell.agerschou@ibt.cas.cz>
2024-11-20 12:32:31 +0100

    made file uploading a synchronous process to be able to trigger update by frontend

--- common/services/files/__init__.py
+++ common/services/files/__init__.py
@@ -1,8 +1,11 @@
 from invenio_records_resources.services import FileServiceConfig
+from invenio_records_resources.services.files.components.metadata import FileMetadataComponent
+from invenio_records_resources.services.files.components.content import FileContentComponent
+
 from oarepo_runtime.services.config.service import PermissionsPresetsConfigMixin
 
 from mbdb_parsing.mst import MocProcessor, XlxsProcessor
-
+from common.services.files.synchronous_file_processing import SynchronousFileProcessorComponent
 
 class MstFileServiceConfigWithProcessors(PermissionsPresetsConfigMixin, FileServiceConfig):
     file_processors = [
@@ -11,5 +14,7 @@ class MstFileServiceConfigWithProcessors(PermissionsPresetsConfigMixin, FileServ
     ]
     components = [
         *PermissionsPresetsConfigMixin.components,
-        *FileServiceConfig.components,
+        FileMetadataComponent,
+        FileContentComponent,
+        SynchronousFileProcessorComponent,
     ]
