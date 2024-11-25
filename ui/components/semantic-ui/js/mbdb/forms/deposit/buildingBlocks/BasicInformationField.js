5b864c28567fb2691034961c80993d3bf7b37a3b
Terezie <terezie.prchalova1@gmail.com>
2024-11-25 12:21:50 +0100

    basic information required

--- ui/components/semantic-ui/js/mbdb/forms/deposit/buildingBlocks/BasicInformationField.js
+++ ui/components/semantic-ui/js/mbdb/forms/deposit/buildingBlocks/BasicInformationField.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Tooltip from "@material-ui/core/Tooltip";
-import { Typography } from "@material-ui/core";
 import FormWrapper from "./FormWrapper";
 import { VocabularyRemoteSelectField } from "@js/oarepo_vocabularies";
 import { useFieldData } from "@js/oarepo_ui";
@@ -15,59 +13,49 @@ function BasicInformationField({ name, colorSchema }) {
         colorSchema={colorSchema}
         headline="Basic information"
         tooltip="Basic information about the chemical. Note that this information is based on PubChem records"
-        >
-          <div className="flex mb-2">
-            <div className="mr-2 mt-0.5">Check your chemistry</div>
-              <div>
-                <a
-                  className="flex justify-center py-1 mr-2 px-4 bg-dark rounded-full text-white hover:bg-secondary hover:text-dark transition-all"
-                  href="https://pubchem.ncbi.nlm.nih.gov/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  PubChem
-                </a>
-              </div>
-              <div>
-                <a
-                  className="flex justify-center py-1 px-4 bg-dark rounded-full text-white hover:bg-secondary hover:text-dark transition-all"
-                  href="https://www.ebi.ac.uk/chembl/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  ChEMBL
-                </a>
-              </div>
-            </div>
-          <div className="flex">
-            <div>
-              <VocabularyRemoteSelectField
+        required
+      >
+        <div className="flex mb-2">
+          <div className="mr-2 mt-0.5">Check your chemistry</div>
+          <div>
+            <a
+              className="flex justify-center py-1 mr-2 px-4 bg-dark rounded-full text-white hover:bg-secondary hover:text-dark transition-all"
+              href="https://pubchem.ncbi.nlm.nih.gov/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              PubChem
+            </a>
+          </div>
+          <div>
+            <a
+              className="flex justify-center py-1 px-4 bg-dark rounded-full text-white hover:bg-secondary hover:text-dark transition-all"
+              href="https://www.ebi.ac.uk/chembl/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              ChEMBL
+            </a>
+          </div>
+        </div>
+        <div className="flex">
+          <div>
+            <VocabularyRemoteSelectField
               overriddenComponents={{
-                  "VocabularyRemoteSelect.ext.ResultsList.item":
+                "VocabularyRemoteSelect.ext.ResultsList.item":
                   RORInstitutionResultListItem,
               }}
               vocabulary="chemicals"
               fieldPath={name}
               modalHeader={
-                  getFieldData({
-                      fieldPath: name,
-                      fieldRepresentation: "text",
-                  }).label
+                getFieldData({
+                  fieldPath: name,
+                  fieldRepresentation: "text",
+                }).label
               }
-              />
-            </div>
-              <div className="text-accent ml-1">
-                <Tooltip
-                  title={
-                    <Typography style={{color: "white", fontSize: 13}}>
-                      This field is required and cannot be left blank or unset
-                    </Typography>
-                  }
-                >
-                  <span>*</span>
-                </Tooltip>
-              </div>
-            </div>
+            />
+          </div>
+        </div>
       </FormWrapper>
     </>
   );
--- ui/components/semantic-ui/js/mbdb/forms/deposit/buildingBlocks/FormWrapper.js
+++ ui/components/semantic-ui/js/mbdb/forms/deposit/buildingBlocks/FormWrapper.js
@@ -2,7 +2,7 @@ import React from "react";
 import Tooltip from "@material-ui/core/Tooltip";
 import { Typography } from "@material-ui/core";
 
-function FormWrapper({ headline, children, colorSchema, tooltip }) {
+function FormWrapper({ headline, children, colorSchema, tooltip, required }) {
   return (
     <>
       <div
@@ -13,6 +13,20 @@ function FormWrapper({ headline, children, colorSchema, tooltip }) {
         {headline && (
           <div className="flex">
             <div className="font-JostMedium text-18px mb-2">{headline}</div>
+            {required && (
+              <div className="text-accent ml-1">
+                <Tooltip
+                  title={
+                    <Typography style={{ color: "white", fontSize: 13 }}>
+                      This field is required and cannot be left blank or unset
+                    </Typography>
+                  }
+                  arrow
+                >
+                  <span>*</span>
+                </Tooltip>
+              </div>
+            )}
             {tooltip && (
               <div className="ml-1 -mt-1">
                 <Tooltip
