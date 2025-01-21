8de45361438f84252524324b265b25e94031ae62
Terezie <terezie.prchalova1@gmail.com>
2025-01-21 11:20:54 +0100

    Added fields

--- ui/components/semantic-ui/js/mbdb/forms/deposit/general/result/CorrectionOfActiveConcentration.js
+++ ui/components/semantic-ui/js/mbdb/forms/deposit/general/result/CorrectionOfActiveConcentration.js
@@ -3,15 +3,32 @@ import ValueUnit from "../../buildingBlocks/ValueUnit";
 import ValueError from "../../buildingBlocks/ValueError";
 import OptionalField from "../../buildingBlocks/OptionalField";
 import CreateUuid from "../../buildingBlocks/CreateUuid";
+import UseDefault from "../../buildingBlocks/UseDefault";
+import ArrayField from "../../buildingBlocks/ArrayField";
+import FormWrapper from "../../buildingBlocks/FormWrapper";
+import EntityInvolved from "./EntityInvolved";
+import CustomField from "../../buildingBlocks/CustomField";
 
 function CorrectionOfActiveConcentration({ name }) {
   CreateUuid(name);
 
   const unitOptions = [{ value: "unitless", label: "unitless" }];
 
+  const fieldNameEntityInvolved = "entities_involved";
+  UseDefault(`${name}.${fieldNameEntityInvolved}`, [{}]);
+
   return (
     <>
       <div className="flex mb-3">
+        <div className="mr-3">
+          <CustomField
+            name={name}
+            required
+            fieldName="name"
+            label="Name"
+            tooltip="Descriptive name (id) of the result (e.g. Kd of Lysozyme and VHH2). Must be unique within a record"
+          />
+        </div>
         <div>
           <ValueUnit
             options={unitOptions}
@@ -23,19 +40,31 @@ function CorrectionOfActiveConcentration({ name }) {
           />
         </div>
       </div>
-      <div>
-        <OptionalField
-          name={name}
-          label="Value error"
-          fieldName="value_error"
-          tooltip="The expected error of the result in terms of a 95 % confidence interval"
-          renderChild={({ optionalFieldName }) => (
-            <div>
-              <ValueError name={optionalFieldName} colorSchema="light" />
-            </div>
-          )}
-        />
-      </div>
+      <OptionalField
+        name={name}
+        label="Value error"
+        fieldName="value_error"
+        tooltip="The expected error of the result in terms of a 95 % confidence interval"
+        renderChild={({ optionalFieldName }) => (
+          <ValueError name={optionalFieldName} colorSchema="light" />
+        )}
+      />
+      <ArrayField
+        name={name}
+        label="Entity involved"
+        fieldName={fieldNameEntityInvolved}
+        required
+        tooltip="List of chemical or molecular assemblies the result describes and how many copies of each are involved"
+        renderChild={({ arrayName, index }) => (
+          <FormWrapper
+            colorSchema="light"
+            headline={`Entity involved ${index + 1}`}
+            tooltip="List of chemical or molecular assemblies the result describes and how many copies of each are involved"
+          >
+            <EntityInvolved name={`${arrayName}.${index}`} />
+          </FormWrapper>
+        )}
+      />
     </>
   );
 }
