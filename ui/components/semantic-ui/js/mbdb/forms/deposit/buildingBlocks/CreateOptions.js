d83a49c0aeb6d03155bbd24763b6f9764f4f0f28
TereziePrchalova <88495359+TereziePrchalova@users.noreply.github.com>
2025-01-21 12:10:30 +0100

    excluding undefined names (#395)

--- ui/components/semantic-ui/js/mbdb/forms/deposit/buildingBlocks/CreateOptions.js
+++ ui/components/semantic-ui/js/mbdb/forms/deposit/buildingBlocks/CreateOptions.js
@@ -1,18 +1,25 @@
 function CreateOptions(list, label) {
-    if (list && list.some(item => item.name !== undefined || item.id !== undefined)) {
-        if (list.some(item => item.name === undefined)) {
-            return [{ label }];
-        }
-        return list.map(item => {
-            return {
-                id: item.id, 
-                value: item.name,
-                label: item.name,
-            };
-        });
-    } else {
-        return [{ label }];
+  if (
+    list &&
+    list.length >= 1 &&
+    list.some((item) => item.name !== undefined || item.id !== undefined)
+  ) {
+    const filteredList = list.filter((item) => item.name !== undefined);
+
+    if (filteredList.length === 0) {
+      return [{ label }];
     }
+
+    return filteredList.map((item) => {
+      return {
+        id: item.id,
+        value: item.name,
+        label: item.name,
+      };
+    });
+  } else {
+    return [{ label }];
+  }
 }
 
 export default CreateOptions;
