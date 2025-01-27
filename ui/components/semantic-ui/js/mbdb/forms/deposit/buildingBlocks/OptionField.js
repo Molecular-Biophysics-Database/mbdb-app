f9baa91fd25f51d2e8e139bbbbce66fb3d3bf038
Terezie <terezie.prchalova1@gmail.com>
2025-01-27 14:51:46 +0100

    option field fixed

--- ui/components/semantic-ui/js/mbdb/forms/deposit/buildingBlocks/OptionField.js
+++ ui/components/semantic-ui/js/mbdb/forms/deposit/buildingBlocks/OptionField.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useField } from "formik";
+import { useField, useFormikContext } from "formik";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
@@ -20,9 +20,18 @@ function OptionField({
   const nameOptionField =
     fieldName !== undefined ? `${name}.${fieldName}` : `${name}`;
   const [field, meta, helpers] = useField(nameOptionField);
+  const { setFieldValue } = useFormikContext();
 
   const currentValue =
     typeof field.value === "object" ? field.value.name : field.value || "";
+
+  // Determine if the current value is valid (exists in options)
+  const isValidValue = options.some((option) => option.value === currentValue);
+
+  // Automatically reset value if it is invalid
+  if (!isValidValue && currentValue) {
+    setFieldValue(nameOptionField, undefined, false);
+  }
 
   return (
     <div className="flex">
@@ -31,15 +40,16 @@ function OptionField({
           <InputLabel>{label}</InputLabel>
           <Select
             {...field}
-            value={currentValue}
+            value={isValidValue ? currentValue : ""}
             onChange={(event) => {
               const selectedOption = options.find(
                 (option) => option.value === event.target.value
               );
+
               helpers.setValue(
-                selectedOption.id !== undefined
+                selectedOption?.id !== undefined
                   ? { name: selectedOption.value, id: selectedOption.id }
-                  : selectedOption.value
+                  : selectedOption?.value || ""
               );
             }}
             label={label}
