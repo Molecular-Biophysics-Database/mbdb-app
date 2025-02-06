f100b1f5b792ca7b4130ea084a6096e208c8cbb6
TereziePrchalova <88495359+TereziePrchalova@users.noreply.github.com>
2025-02-06 13:30:41 +0100

    Save (#416)

--- ui/spr/semantic-ui/templates/spr/form/FormFieldsContainer.jsx
+++ ui/spr/semantic-ui/templates/spr/form/FormFieldsContainer.jsx
@@ -40,6 +40,9 @@ function FormFieldsContainer() {
 
   useEffect(() => {
     save(true);
+  }, []);
+
+  useEffect(() => {
     const selectedTab = location?.state?.selectedTab || "record-information";
     setState({ selected: selectedTab });
   }, [location]);
