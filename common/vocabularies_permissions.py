0caa04afce390e386ab00749b1cda49683bf3025
Emil Dandanell Agerschou <emildandanell.agerschou@ibt.cas.cz>
2025-02-14 13:08:12 +0100

    Vocabulary UI update
    * Updated instrument create permission to include editors
    * Refactored how ID field are added to UI
    * Updated instrument fields to be picked up by the UI
    * Added ID fields to UI of body_fluids, cell_fractions, environment_types, and products

--- common/vocabularies_permissions.py
+++ common/vocabularies_permissions.py
@@ -1,16 +1,19 @@
 from invenio_records_permissions.generators import AuthenticatedUser, SystemProcess
 from oarepo_runtime.services.config.permissions_presets import ReadOnlyPermissionPolicy
-
+from common.workflows.custom_generators import UserWithRole
 
 class FineGrainedVocabularyPermissionPolicy(ReadOnlyPermissionPolicy):
+    """Permission policy for vocabularies"""
+    # Roles that are potentially able to create vocabularies
+    can_create = [AuthenticatedUser(), SystemProcess()]
+    # Roles which can create specific vocabularies
     can_create_affiliations = [AuthenticatedUser(), SystemProcess()]
-    can_create_cell_fractions = [AuthenticatedUser(), SystemProcess()]
     can_create_body_fluids = [AuthenticatedUser(), SystemProcess()]
+    can_create_cell_fractions = [AuthenticatedUser(), SystemProcess()]
     can_create_chemicals = [AuthenticatedUser(), SystemProcess()]
     can_create_environment_types = [AuthenticatedUser(), SystemProcess()]
-    can_create_instruments = [SystemProcess()]
-    can_create_languages = [SystemProcess()]
     can_create_grants = [AuthenticatedUser(), SystemProcess()]
+    can_create_instruments = [SystemProcess(), UserWithRole("editor")]
+    can_create_languages = [SystemProcess()]
     can_create_organisms = [SystemProcess()]
     can_create_products = [AuthenticatedUser(), SystemProcess()]
-    can_create = [AuthenticatedUser(), SystemProcess()]
