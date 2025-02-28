e5d94da34cb4d0ec5d24247ca5e3632cf062123f
Emil Dandanell Agerschou <emildandanell.agerschou@ibt.cas.cz>
2025-02-28 15:35:33 +0100

    models updated

--- itc/services/records/service.py
+++ itc/services/records/service.py
@@ -1,5 +1,7 @@
 from invenio_rdm_records.services.services import RDMRecordService
 
+from common.services.records.service import AddWorkflowServiceMixin
 
-class ItcService(RDMRecordService):
+
+class ItcService(AddWorkflowServiceMixin, RDMRecordService):
     """ItcRecord service."""
--- spr/services/records/service.py
+++ spr/services/records/service.py
@@ -1,5 +1,7 @@
 from invenio_rdm_records.services.services import RDMRecordService
 
+from common.services.records.service import AddWorkflowServiceMixin
 
-class SprService(RDMRecordService):
+
+class SprService(AddWorkflowServiceMixin, RDMRecordService):
     """SprRecord service."""
