da0856cbbdbe2d7a1a885bfcf330455103f78566
jplucarova <192219926+jplucarova@users.noreply.github.com>
2025-09-04 10:59:09 +0200

    Cal add spec (#477)
    * Copy updated definitions.yaml
    
    * Compile mp model
    
    * Compile bli, itc, mst models
    
    * Compile spr model

--- spr/services/records/schema.py
+++ spr/services/records/schema.py
@@ -133,7 +133,7 @@ class GeneralParametersSchema(DictOnlySchema):
         validate=[ma.validate.Length(min=1)],
     )
 
-    schema_version = ma_fields.String(required=True, validate=[OneOf(["0.14.1"])])
+    schema_version = ma_fields.String(required=True, validate=[OneOf(["0.14.2"])])
 
 
 class ChemicalEnvironmentsItemSchema(DictOnlySchema):
@@ -738,7 +738,7 @@ class MethodSpecificParametersSchema(DictOnlySchema):
         validate=[ma.validate.Length(min=1)],
     )
 
-    schema_version = ma_fields.String(required=True, validate=[OneOf(["0.10.1"])])
+    schema_version = ma_fields.String(required=True, validate=[OneOf(["0.10.2"])])
 
     sensor = ma_fields.Nested(lambda: SensorSchema(), required=True)
 
--- spr/services/records/ui_schema.py
+++ spr/services/records/ui_schema.py
@@ -94,7 +94,7 @@ class GeneralParametersUISchema(DictOnlySchema):
 
     results = ma_fields.List(ma_fields.Nested(lambda: ResultsItemUISchema()))
 
-    schema_version = ma_fields.String(required=True, validate=[OneOf(["0.14.1"])])
+    schema_version = ma_fields.String(required=True, validate=[OneOf(["0.14.2"])])
 
 
 class ChemicalEnvironmentsItemUISchema(DictOnlySchema):
@@ -913,7 +913,7 @@ class MethodSpecificParametersUISchema(DictOnlySchema):
         ma_fields.Nested(lambda: MeasurementsItemUISchema()), required=True
     )
 
-    schema_version = ma_fields.String(required=True, validate=[OneOf(["0.10.1"])])
+    schema_version = ma_fields.String(required=True, validate=[OneOf(["0.10.2"])])
 
     sensor = ma_fields.Nested(lambda: SensorUISchema(), required=True)
 
