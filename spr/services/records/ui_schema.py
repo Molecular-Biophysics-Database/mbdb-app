c240695eba8cf1eefb58060318c55d33046e0be2
Jitka Plucarova <192219926+jplucarova@users.noreply.github.com>
2025-09-18 17:11:53 +0200

    Compile spr model

--- spr/services/records/ui_schema.py
+++ spr/services/records/ui_schema.py
@@ -94,7 +94,7 @@ class GeneralParametersUISchema(DictOnlySchema):
 
     results = ma_fields.List(ma_fields.Nested(lambda: ResultsItemUISchema()))
 
-    schema_version = ma_fields.String(required=True, validate=[OneOf(["0.14.2"])])
+    schema_version = ma_fields.String(required=True, validate=[OneOf(["0.15.0"])])
 
 
 class ChemicalEnvironmentsItemUISchema(DictOnlySchema):
