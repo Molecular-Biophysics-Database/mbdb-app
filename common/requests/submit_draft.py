18eab5207084bdf5272602d324a52bd063b51005
Tim Kadlec <tim@mwq.cz>
2025-12-04 19:32:49 +0100

    create notification builders, assign them to their actions and register them in NOTIFICATION_BUILDERS

--- common/requests/submit_draft.py
+++ common/requests/submit_draft.py
@@ -4,9 +4,96 @@ from oarepo_runtime.i18n import lazy_gettext as _
 from oarepo_requests.types import ModelRefTypes
 from oarepo_requests.types.generic import NonDuplicableOARepoRequestType
 from oarepo_runtime.datastreams.utils import get_record_service_for_record
+from oarepo_requests.actions.generic import OARepoSubmitAction, OARepoAcceptAction, OARepoDeclineAction
+from invenio_notifications.services.uow import NotificationOp
+from oarepo_requests.notifications.generators import EntityRecipient
+from oarepo_requests.notifications.builders.oarepo import OARepoRequestActionNotificationBuilder
+
+class DraftRequestSubmitReceiverNotificationBuilder(OARepoRequestActionNotificationBuilder):
+    type = "draft-request-receiver.submit"
+    # Receivers as recipients (reviewers)
+    recipients = [EntityRecipient(key="request.receiver")]
+
+class DraftRequestSubmitCreatorNotificationBuilder(OARepoRequestActionNotificationBuilder):
+    type = "draft-request-creator.submit"
+    # User that created/sent the request (author of the record)
+    recipients = [EntityRecipient(key="request.created_by")]
+
+class DraftRequestAcceptCreatorNotificationBuilder(OARepoRequestActionNotificationBuilder):
+    type = "draft-request-accept-creator.submit"
+    # Authors as recipients
+    recipients = [EntityRecipient(key="request.created_by")]
+
+class DraftRequestDeclineCreatorNotificationBuilder(OARepoRequestActionNotificationBuilder):
+    type = "draft-request-decline-creator.submit"
+    recipients = [EntityRecipient(key="request.created_by")]
+
+class SubmitDraftAction(OARepoSubmitAction):
+    """Submit draft action."""
+
+    def apply(
+            self,
+            identity,
+            state,
+            uow,
+            *args,
+            **kwargs,
+    ) -> None:
+        # Send notification to Receivers
+        uow.register(
+            NotificationOp(
+                DraftRequestSubmitReceiverNotificationBuilder.build(request=self.request)
+            )
+        )
+
+        # Send notification to Creators
+        uow.register(
+            NotificationOp(
+                DraftRequestSubmitCreatorNotificationBuilder.build(request=self.request)
+            )
+        )
+        return super().apply(identity, state, uow, *args, **kwargs)
+
+class AcceptDraftAction(OARepoAcceptAction):
+    """Accept draft action."""
+
+    def apply(
+            self,
+            identity,
+            state,
+            uow,
+            *args,
+            **kwargs,
+    ) -> None:
+        # Send notification to Creators
+        uow.register(
+            NotificationOp(
+                DraftRequestAcceptCreatorNotificationBuilder.build(request=self.request)
+            )
+        )
+        return super().apply(identity, state, uow, *args, **kwargs)
+
+class DeclineDraftAction(OARepoDeclineAction):
+    """Decline draft action."""
+
+    def apply(
+            self,
+            identity,
+            state,
+            uow,
+            *args,
+            **kwargs,
+    ) -> None:
+        request_event = super().apply(identity, state, uow, *args, **kwargs)
+        # Send notification to Creators
+        uow.register(
+            NotificationOp(
+                DraftRequestDeclineCreatorNotificationBuilder.build(request=self.request)
+            )
+        )
+        return super().apply(identity, state, uow, *args, **kwargs)
 
 # Request
-#
 class SubmitDraftRequestType(NonDuplicableOARepoRequestType):
     """
     Custom submit draft request that validates the draft upon submission. The
@@ -16,11 +103,17 @@ class SubmitDraftRequestType(NonDuplicableOARepoRequestType):
     type_id = "submit_draft"
     name = _("Submit")
 
+    # Modal popup
+    dangerous = True
+
     @classmethod
     @property
     def available_actions(cls):
         return {
             **super().available_actions,
+            "submit": SubmitDraftAction,
+            "accept": AcceptDraftAction,
+            "decline": DeclineDraftAction,
         }
 
     receiver_can_be_none = False
