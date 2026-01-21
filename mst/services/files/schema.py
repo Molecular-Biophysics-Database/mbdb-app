348ff0ecec746dff5b10948a6ba6fd24101c80fa
Tim Kadlec <tim@mwq.cz>
2026-01-21 12:04:33 +0100

    move metadata conditions to MstMetadataSchema and nest it into MstFileSchema

--- mst/services/files/schema.py
+++ mst/services/files/schema.py
@@ -7,38 +7,29 @@ from marshmallow.validate import OneOf
 from oarepo_runtime.services.schema.marshmallow import DictOnlySchema
 from oarepo_runtime.services.schema.validation import validate_date
 
-
-class MstFileSchema(InvenioFileSchema):
+class MstFileMetadataSchema(ma.Schema):
     class Meta:
         unknown = ma.RAISE
 
     content_type = ma_fields.String(
-        required=True, validate=[OneOf(["text", "binary", "text and binary"])]
+        required=True, validate=OneOf(["text", "binary", "text and binary"])
     )
 
     context = ma_fields.String(
         required=True,
-        validate=[
-            OneOf(
-                [
-                    "raw measurement data",
-                    "derived measurement data",
-                    "quality control report",
-                ]
-            )
-        ],
+        validate=OneOf([
+            "raw measurement data",
+            "derived measurement data",
+            "quality control report",
+        ]),
     )
 
-    created = ma_fields.String(dump_only=True, validate=[validate_date("%Y-%m-%d")])
-
-    creation_date = ma_fields.String(
-        required=True, validate=[validate_date("%Y-%m-%d")]
-    )
+    creation_date = ma_fields.String(required=True, validate=[validate_date("%Y-%m-%d")])
 
     description = ma_fields.String()
 
     originates_from = ma_fields.String(
-        required=True, validate=[OneOf(["Instrument software", "User", "MBDB"])]
+        required=True, validate=OneOf(["Instrument software", "User", "MBDB"])
     )
 
     processing_steps = ma_fields.List(
@@ -49,6 +40,13 @@ class MstFileSchema(InvenioFileSchema):
 
     recommended_software = ma_fields.String()
 
+class MstFileSchema(InvenioFileSchema):
+    class Meta:
+        unknown = ma.RAISE
+
+    metadata = ma_fields.Nested(MstFileMetadataSchema, required=True)
+
+    created = ma_fields.String(dump_only=True, validate=[validate_date("%Y-%m-%d")])
     updated = ma_fields.String(dump_only=True, validate=[validate_date("%Y-%m-%d")])
 
 
