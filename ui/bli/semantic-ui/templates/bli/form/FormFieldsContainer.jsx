754082984253dcdb2f4eba3f6b2d4d02c338aefc
Terezie <terezie.prchalova1@gmail.com>
2026-07-09 15:21:59 +0200

    BLI fix stale save status

--- ui/bli/semantic-ui/templates/bli/form/FormFieldsContainer.jsx
+++ ui/bli/semantic-ui/templates/bli/form/FormFieldsContainer.jsx
@@ -19,7 +19,7 @@ import { FormContext } from "./FormProvider";
 function FormFieldsContainer() {
   const community = new URLSearchParams(location.search).get('community');
 
-  const { tabs, selectedTab, setSelectedTab, setShowErrors, setFileUploadErrors } = useContext(FormContext);
+  const { tabs, selectedTab, setSelectedTab, setFileUploadErrors, setSavedAt, setIsSaving } = useContext(FormContext);
   const { save, values: recordMetadata } = useDepositApiClient();
   const { values, setErrors } = useFormikContext();
 
@@ -63,8 +63,10 @@ function FormFieldsContainer() {
       if (tag === "textarea" || target?.isContentEditable) return;
 
       e.preventDefault();
+      setIsSaving(true);
       await handleSaveMetadataAndFiles();
-      setShowErrors(true);
+      setSavedAt(Date.now());
+      setIsSaving(false);
     };
 
     window.addEventListener("keydown", handleKeyDown);
@@ -72,7 +74,7 @@ function FormFieldsContainer() {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [handleSaveMetadataAndFiles, setShowErrors]);
+  }, [handleSaveMetadataAndFiles, setSavedAt, setIsSaving]);
 
   return (
     <>
