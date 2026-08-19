7afcc6a70810c63d646f416342b32b825e0993fa
timkadlec <tim@mwq.cz>
2026-08-19 15:09:48 +0200

    itc: add itc model

---
+++ models/itc/__init__.py
@@ -0,0 +1,13 @@
+"""Model for itc.
+
+This package contains the model definition for itc.
+To change model's metadata, edit the metadata.yaml file.
+To change presets, add serialization/deserialization formats
+and similar operations please see the model.py file.
+"""
+
+from __future__ import annotations
+
+from .model import itc_model
+
+__all__ = ( "itc_model", )
