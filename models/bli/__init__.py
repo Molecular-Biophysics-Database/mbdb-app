f5352dba6482ff6faa7cde21cdd0d915b04f1352
timkadlec <tim@mwq.cz>
2026-08-19 15:11:07 +0200

    bli: add bli model

---
+++ models/bli/__init__.py
@@ -0,0 +1,13 @@
+"""Model for bli.
+
+This package contains the model definition for bli.
+To change model's metadata, edit the metadata.yaml file.
+To change presets, add serialization/deserialization formats
+and similar operations please see the model.py file.
+"""
+
+from __future__ import annotations
+
+from .model import bli_model
+
+__all__ = ( "bli_model", )
